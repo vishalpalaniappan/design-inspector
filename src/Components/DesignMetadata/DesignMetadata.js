e4d221e75c300e4e85fba93b3d9ab1145db19e6e
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-02-19 14:15:20 -0500

    Add initial files.

---
+++ src/Components/DesignMetadata/DesignMetadata.js
@@ -0,0 +1,16 @@
+import React, {useContext, useEffect, useState} from "react";
+
+import "./DesignMetadata.scss";
+
+DesignMetadata.propTypes = {
+};
+
+/**
+ * Component to display the design metadata.
+ * @return {JSX.Element}
+ */
+export function DesignMetadata ({}) {
+    return (
+        <></>
+    );
+}
---
+++ src/Components/FlowDiagram/FlowDiagram.js
@@ -0,0 +1,62 @@
+import React, {useContext, useEffect, useState} from "react";
+
+import {
+    Controls,
+    ReactFlow,
+    ReactFlowProvider,
+    useEdgesState,
+    useNodesState,
+    useReactFlow
+} from "@xyflow/react";
+import PropTypes from "prop-types";
+
+import "@xyflow/react/dist/style.css";
+
+Flow.propTypes = {
+    tree: PropTypes.object,
+};
+
+/**
+ * Flow component which reners the dependency graph.
+ * @param {Object} tree
+ * @return {JSX.Element}
+ */
+export function Flow ({tree}) {
+    const {fitView} = useReactFlow();
+    const [nodes, setNodes, onNodesChange] = useNodesState([]);
+    const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+
+    useEffect(() => {
+        if (tree) {
+            fitView();
+        }
+    }, [tree]);
+
+    return (
+        <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            nodesDraggable={false}
+            colorMode={"dark"}
+            fitView
+        >
+            <Controls />
+        </ReactFlow>
+    );
+};
+
+/**
+ *
+ * @return {JSX.Element}
+ */
+export function FlowDiagram () {
+    const [tree] = useState();
+
+    return (
+        <ReactFlowProvider>
+            <Flow tree={tree} />
+        </ReactFlowProvider>
+    );
+};
