7eb6745c3b2902e55c04951f7938b21193945801
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-02-23 12:46:00 -0500

    Remove unncessary comments.

--- server/index.js
+++ server/index.js
@@ -4,17 +4,8 @@ import http from "node:http";
 import handleWSConnection from "./websocket.js";
 import { server as WebSocketServer } from "websocket";
 
-/*
-    1. Create express server
-    2. Route / to index.html in the public folder
-    3. Create Websocket server on same port
-    4. Send request for websocket connection to websocket connection handler.
-*/
-
-
 //Port on which the http and websocket connections are established
 const PORT = 3002;
-
 const app = express();
 
 app.use((req, res, next) => {
@@ -25,11 +16,8 @@ app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'public') + '/index.html')
 });
 
-
 //Create the HTTP server
 const server = http.createServer(app);
-
-//Listen on the specified port
 server.listen(PORT);
 
 //Create a new websocket server on the same port as the http connection
@@ -37,7 +25,6 @@ const wsServer = new WebSocketServer({
     httpServer: server
 });
 
-//When websocket connection request is received, send it to the handleWSConnection function
 wsServer.on('request', function(request) {
     console.log((new Date()) + ' Recieved a new connection from origin ' + request.origin + '.');
     handleWSConnection(request);
