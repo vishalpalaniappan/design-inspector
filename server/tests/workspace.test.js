04f96d91d7b9ef1c6d4b8af5991b4342d31b937d
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-20 16:26:29 -0400

    Add sample workspace output to temp folder so it can be imported into library component.

--- server/tests/workspace.test.js
+++ server/tests/workspace.test.js
@@ -1,12 +1,14 @@
-// src/add.test.js
 import { describe, it, expect } from 'vitest';
+import {writeFile} from "fs/promises"
 import loadDir from '../src/loadDir';
+import {resolve} from "path"
 import path from "node:path";
 
 describe('load workspace', () => {
     it('loads workspace', async () => {
         const workspacePath = path.join(process.cwd(), 'workspace');
         const result = await loadDir(workspacePath);
-        console.log(JSON.stringify(result));
+        const filePath2 = resolve(__dirname, "./temp/workspace_sample.json")
+        await writeFile(filePath2, JSON.stringify(result));
     });
 });
