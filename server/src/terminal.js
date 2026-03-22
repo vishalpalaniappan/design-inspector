89b40529399e4529c2f0ce4d9f9f073da83aed07
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-22 13:12:50 -0400

    Add skeleton for event based terminal class.

---
+++ server/src/terminal.js
@@ -0,0 +1,27 @@
+import pty from "node-pty";
+import os from "os";
+import { EventEmitter } from "events";
+
+export class TerminalSession {
+    constructor(onData, onExit) { }
+
+    start() {
+
+    }
+
+    write() {
+
+    }
+
+    resize() {
+
+    }
+
+    stop() {
+
+    }
+
+    restart() {
+
+    }
+}
