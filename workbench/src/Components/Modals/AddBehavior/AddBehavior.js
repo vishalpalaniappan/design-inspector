d9e694b40f8f60b54ab97476916ac2a5e37e28a1
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-28 20:07:10 -0400

    Add simple behavior info and add behavior components.

--- workbench/src/Components/Modals/AddBehavior/AddBehavior.js
+++ workbench/src/Components/Modals/AddBehavior/AddBehavior.js
@@ -1,6 +1,7 @@
 import React, {useContext, useEffect, useState} from "react";
 
 import PropTypes from "prop-types";
+import {useLayoutEventPublisher} from "ui-layout-manager-dev";
 
 import {useDalEngine} from "../../../Providers/GlobalProviders";
 
@@ -16,14 +17,39 @@ AddBehavior.propTypes = {
  */
 export function AddBehavior ({close}) {
     const {engine} = useDalEngine();
+    const [behavior, setBehavior] = useState("");
+
+    const publish = useLayoutEventPublisher();
 
     useEffect(() => {
 
     }, [engine]);
 
+    const submit = () => {
+        if (behavior.trim() === "") {
+            return;
+        }
+        publish({
+            type: "add:behavior",
+            payload: behavior,
+            source: "add-behavior-modal",
+        });
+        close();
+    };
+
     return (
         <div className="add-behavior-modal">
-            <h2 onClick={close}>Add Behavior</h2>
+            <div className="behavior-name-label">
+                <span>Behavior Name:</span>
+            </div>
+            <div className="behavior-name-input">
+                <input
+                    value={behavior}
+                    onChange={(e) => setBehavior(e.target.value)}/>
+                <div className="behavior-name-submit">
+                    <button onClick={submit}>Add Behavior</button>
+                </div>
+            </div>
         </div>
     );
 }
