6eab8a36e671566ce6438119ec957eb2b7ec5a54
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-03-29 18:57:03 -0400

    Improve styling of add behavior button.

--- workbench/src/Components/Modals/AddBehavior/AddBehavior.js
+++ workbench/src/Components/Modals/AddBehavior/AddBehavior.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, {useContext, useEffect, useRef, useState} from "react";
 
 import PropTypes from "prop-types";
 import {useLayoutEventPublisher} from "ui-layout-manager-dev";
@@ -18,14 +18,18 @@ AddBehavior.propTypes = {
 export function AddBehavior ({close}) {
     const {engine} = useDalEngine();
     const [behavior, setBehavior] = useState("");
+    const inputRef = useRef(null);
 
     const publish = useLayoutEventPublisher();
 
     useEffect(() => {
-
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
     }, [engine]);
 
-    const submit = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault(); // stops page refresh
         if (behavior.trim() === "") {
             return;
         }
@@ -42,14 +46,15 @@ export function AddBehavior ({close}) {
             <div className="behavior-name-label">
                 <span>Behavior Name:</span>
             </div>
-            <div className="behavior-name-input">
+            <form className="behavior-name-input" onSubmit={handleSubmit}>
                 <input
+                    ref={inputRef}
                     value={behavior}
                     onChange={(e) => setBehavior(e.target.value)}/>
                 <div className="behavior-name-submit">
-                    <button onClick={submit}>Add Behavior</button>
+                    <button type="submit">Add Behavior</button>
                 </div>
-            </div>
+            </form>
         </div>
     );
 }
