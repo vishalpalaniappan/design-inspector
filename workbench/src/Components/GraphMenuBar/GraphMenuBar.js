6e71b81eeb0f0414a2de44c3aaea17ac83bdaf77
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-02 08:49:34 -0400

    Add icon to show graph info.

--- workbench/src/Components/GraphMenuBar/GraphMenuBar.js
+++ workbench/src/Components/GraphMenuBar/GraphMenuBar.js
@@ -1,6 +1,6 @@
 import React, {useCallback, useEffect} from "react";
 
-import {Display, PlusSquare, Trash} from "react-bootstrap-icons";
+import {InfoCircle, PlusSquare, Trash} from "react-bootstrap-icons";
 import {useDispatch} from "react-redux";
 import {useModalManager} from "ui-layout-manager-dev";
 
@@ -8,6 +8,7 @@ import {useDalEngine} from "../../Providers/GlobalProviders";
 import {incrementCounter, setSelectedBehavior, setSelectedGraph} from "../../Store/appSlice";
 import {useGraphs, useSelectedGraph} from "../../Store/useAppSelection";
 import {AddGraph} from "../Modals/AddGraph";
+import {ShowInfo} from "../Modals/ShowInfo";
 
 import "./GraphMenuBar.scss";
 
@@ -52,6 +53,18 @@ export function GraphMenuBar () {
         dispatch(incrementCounter());
     }, [engine, selectedGraph, dispatch]);
 
+    const showInfo = useCallback(() => {
+        openModal({
+            title: "Graph Info",
+            args: {
+                selected: engine.graph,
+            },
+            render: ({close, args}) => {
+                return <ShowInfo close={close} args={args} />;
+            },
+        });
+    }, [engine]);
+
     return (
         <div className="graphMenuBar">
             <div className="leftMenu"></div>
@@ -66,6 +79,7 @@ export function GraphMenuBar () {
                 </select>
                 <PlusSquare title={"Add Graph"} onClick={addgraph} className="icon"/>
                 <Trash title={"Delete Graph"} onClick={deleteGraph} className="icon"/>
+                <InfoCircle title={"Graph Info"} onClick={showInfo} className="icon"/>
             </div>
         </div>
     );
