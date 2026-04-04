a62857690dcd493548a1c7c138a18214c76248d3
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-04 16:42:02 -0400

    Setup thunk file.

---
+++ workbench/src/Store/appThunk.js
@@ -0,0 +1,7 @@
+// Store thunks here with engine to cleanly apply changes to the engine,
+// update state and then make the relevant information available.
+
+export const addParticipant = (engine, participant) => (dispatch, getState) => {
+
+
+};
