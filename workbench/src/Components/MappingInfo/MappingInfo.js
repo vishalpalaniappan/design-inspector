0d73aad859b057c7f2108eaec52758b827093950
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-05 20:03:56 -0400

    Add skeleton for mapping info container.

---
+++ workbench/src/Components/MappingInfo/MappingInfo.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./MappingInfo.scss";
+
+/**
+ * MappingInfo Component
+ * @return {JSX.Element}
+ */
+export function MappingInfo () {
+    return (
+        <div className="mapping-container">
+        </div>
+    );
+}
