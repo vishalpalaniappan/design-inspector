5e2bbe14e713434803abb932f8ded7aa21d58e7e
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-07 16:02:58 -0400

    Create component registry in separate file.

---
+++ workbench/src/Registry.js
@@ -0,0 +1,42 @@
+export const registry = {
+    BehavioralControlGraph: () =>
+        import("./Components/BehavioralControlGraph/BehavioralControlGraph").then((m) => ({
+            default: m.default || m.BehavioralControlGraph,
+        })),
+    EditorContainer: () =>
+        import("./Components/EditorContainer/EditorContainer").then((m) => ({
+            default: m.default || m.EditorContainer,
+        })),
+    FileSelector: () =>
+        import("./Components/FileSelector/FileSelector").then((m) => ({
+            default: m.default || m.FileSelector,
+        })),
+    PtyTerminal: () =>
+        import("./Components/PtyTerminal/PtyTerminal").then((m) => ({
+            default: m.default || m.PtyTerminal,
+        })),
+    ToolBar: () =>
+        import("./Components/ToolBar/ToolBar").then((m) => ({
+            default: m.default || m.ToolBar,
+        })),
+    StatusBar: () =>
+        import("./Components/StatusBar/StatusBar").then((m) => ({
+            default: m.default || m.StatusBar,
+        })),
+    GraphMenuBar: () =>
+        import("./Components/GraphMenuBar/GraphMenuBar").then((m) => ({
+            default: m.default || m.GraphMenuBar,
+        })),
+    NodeInfo: () =>
+        import("./Components/NodeInfo/NodeInfo").then((m) => ({
+            default: m.default || m.NodeInfo,
+        })),
+    SelectedInfo: () =>
+        import("./Components/SelectedInfo/SelectedInfo").then((m) => ({
+            default: m.default || m.SelectedInfo,
+        })),
+    MappingInfo: () =>
+        import("./Components/MappingInfo/MappingInfo").then((m) => ({
+            default: m.default || m.MappingInfo,
+        })),
+};
