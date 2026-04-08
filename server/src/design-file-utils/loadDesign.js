7eb3d0685fa48f9e42f0a2af14280e3c3bf25893
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-08 13:45:01 -0400

    Initialize playground with files from engine when design is loaded.

--- server/src/design-file-utils/loadDesign.js
+++ server/src/design-file-utils/loadDesign.js
@@ -1,5 +1,8 @@
 import fs from 'fs/promises';
 import { resolveDesignPath } from "./validateDesignName.js";
+import {DALEngine} from "dal-engine-core-js-lib-dev";
+import { initPlaygroundFolder } from './initFolders.js';
+import path from 'path';
 
 /**
  * Loads a design from the workspace with the given name.
@@ -10,6 +13,24 @@ async function loadDesign(designName) {
     try {
         const filePath = resolveDesignPath(designName);
         const data = await fs.readFile(filePath, "utf-8");
+
+        // Create engine and deserialize data from file
+        const engine = new DALEngine({
+            name: designName,
+            description: "Default engine",
+        });
+        engine.deserialize(data);
+
+        // Create playground folder if it doesn't exist
+        await initPlaygroundFolder();
+
+        // Write engine files to playground folder
+        const playgroundPath = path.join(process.cwd(), "playground");
+        engine.getFiles().forEach(async (file) => {
+            const filePath = path.resolve(playgroundPath, file.name);
+            await fs.writeFile(filePath, file.content, { flag: "w" });
+        });
+
         return {
             fileName: designName,
             path: filePath,
