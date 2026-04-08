6b551ac2489ab79d6b03fabee67894f3778d2c3e
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-08 15:02:02 -0400

    Setup play button to execute entry point in playground.

--- workbench/src/Components/ToolBar/ToolBar.js
+++ workbench/src/Components/ToolBar/ToolBar.js
@@ -1,10 +1,11 @@
-import React, {useCallback} from "react";
+import React, {useCallback, useEffect} from "react";
 
-import {Floppy, PlusSquare} from "react-bootstrap-icons";
+import {Floppy, PlayFill, PlusSquare} from "react-bootstrap-icons";
 import {useDispatch} from "react-redux";
 import {useModalManager} from "ui-layout-manager-dev";
 
 import {useDalEngine} from "../../Providers/GlobalProviders";
+import {useServer} from "../../Providers/GlobalProviders";
 import {setStatusMsg} from "../../Store/appSlice";
 import {AddBehavior} from "../Modals/AddBehavior";
 
@@ -18,13 +19,22 @@ export function ToolBar () {
     const {openModal} = useModalManager();
     const {engine} = useDalEngine();
     const dispatch = useDispatch();
+    const {sendJsonMessage} = useServer();
+    const [hasEntryPoint, setHasEntryPoint] = React.useState(false);
+
+    useEffect(() => {
+        if (engine) {
+            const entryPoint = engine.implementation.getEntryPoint();
+            setHasEntryPoint(Boolean(entryPoint));
+        }
+    }, [engine]);
 
     const saveGraph = useCallback(() => {
         if (engine) {
             engine.save();
             dispatch(setStatusMsg("Saving design..."));
         }
-    }, [engine]);
+    }, [engine, dispatch]);
 
     const addBehavior = () => {
         openModal({
@@ -34,6 +44,19 @@ export function ToolBar () {
             },
         });
     };
+
+    const runDesign = useCallback(() => {
+        if (sendJsonMessage && engine) {
+            const failureMsg = "Failed to run design. Please ensure an entry point is set.";
+            const cmd = (hasEntryPoint)?
+                engine.implementation.getEntryPoint():
+                `echo "${failureMsg}"`;
+            sendJsonMessage({
+                type: "terminal_input",
+                data: cmd + "\n",
+            });
+        }
+    }, [sendJsonMessage, hasEntryPoint,engine]);
 
     return (
         <div className="toolbarWrapper">
@@ -45,6 +68,13 @@ export function ToolBar () {
                 />
             </div>
             <div className="toolbarContainer bottom">
+                <PlayFill
+                    size={20}
+                    style={{color: hasEntryPoint ? "#4f9160" : "grey"}}
+                    onClick={(e) => runDesign()}
+                    title="Run Design"
+                    className="icon"
+                />
                 <Floppy
                     onClick={(e) => saveGraph()}
                     title="Save Graph"
