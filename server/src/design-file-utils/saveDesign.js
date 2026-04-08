37d17d00bdbb5c6ea1f453072ae55899115a3eb9
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-08 09:34:21 -0400

    Add docs for save design.

--- server/src/design-file-utils/saveDesign.js
+++ server/src/design-file-utils/saveDesign.js
@@ -3,38 +3,55 @@ import path from "node:path";
 import {DALEngine} from "dal-engine-core-js-lib-dev";
 import getMapping from '../getMapping.js';
 
-
+/**
+ * Saves the design to the workspace. However, it first checks
+ * for any python files in the design and updates their mapping
+ * before saving the design. This updated mapping is used by the
+ * client to map the implementation to the design.
+ * 
+ * @param {String} designName Name of the design.
+ * @param {String} data Serialized engine instance.
+ * @returns {Object} Object containing the files in the design.
+ */
 async function saveDesign(designName,  data) {
-    const workspacePath = path.join(process.cwd(), "workspace");
-    const filePath = path.join(workspacePath, designName);
+    try {
+        const workspacePath = path.join(process.cwd(), "workspace");
+        const filePath = path.join(workspacePath, designName);
 
-    const engine = new DALEngine({
-        name: "default",
-        description: "Default engine",
-    });
+        const engine = new DALEngine({
+            name: "default",
+            description: "Default engine",
+        });
 
-    engine.deserialize(data);
+        engine.deserialize(data);
 
-    const pythonFiles = engine.getFiles().filter((file) => {
-        return (file.name.endsWith(".py") && file.updatedContent !== file.content);
-    });
+        const pythonFiles = engine.getFiles().filter((file) => {
+            return (file.name.endsWith(".py") && file.updatedContent !== file.content);
+        });
 
-    await Promise.all(
-        pythonFiles.map(async (file) => {
-            const mapping = await getMapping(file.updatedContent);
-            file.mapping = mapping;
-        })
-    );
+        await Promise.all(
+            pythonFiles.map(async (file) => {
+                const mapping = await getMapping(file.updatedContent);
+                file.mapping = mapping;
+            })
+        );
 
-    engine.getFiles().forEach((file) => {
-        file.content = file.updatedContent;
-    });
+        // This marks the file as not dirty. If the content does not match
+        // the update content, then the file is considered dirty and the UI
+        // will reflect this.
+        engine.getFiles().forEach((file) => {
+            file.content = file.updatedContent;
+        });
 
-    const serializedEngine = engine.serialize();
-    await fs.writeFile(filePath, serializedEngine);
+        const serializedEngine = engine.serialize();
+        await fs.writeFile(filePath, serializedEngine);
 
-    return {
-        files: engine.getFiles()
-    };
+        return {
+            files: engine.getFiles()
+        };
+    } catch (err) {
+        throw new Error(err);
+    }
 }
+
 export default saveDesign;
