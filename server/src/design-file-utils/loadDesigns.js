14cc8b7184dd3805841f00522616bbfdb5d97c69
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-08 13:37:36 -0400

    Use extracted function to make sure workspace folder exists before loading designs.

--- server/src/design-file-utils/loadDesigns.js
+++ server/src/design-file-utils/loadDesigns.js
@@ -1,6 +1,6 @@
 import path from "node:path";
 import fs from 'fs/promises';
-import { randomUUID } from 'crypto';
+import { initWorkspaceFolder } from "./initFolders";
 
 /**
  * Returns the list of designs in the workspace.
@@ -8,17 +8,8 @@ import { randomUUID } from 'crypto';
  */
 async function loadDesigns() {
     try {
+        await initWorkspaceFolder();
         const workspacePath = path.join(process.cwd(), "workspace");
-        try {
-            await fs.mkdir(workspacePath, { recursive: true });
-        } catch (err) {
-            if (err.code === "EEXIST") {
-                // Directory already exists.
-            } else {
-                throw err;
-            }
-        }
-
         const entries = await fs.readdir(workspacePath, { withFileTypes: true });
         const designs = entries.map((entry) => {
             if (!entry.isDirectory() && entry.name.endsWith(".dal")) {
