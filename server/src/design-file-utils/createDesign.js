b5db093149c67f827bef5df528877843b66c5f36
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-08 09:18:33 -0400

    Add doc to createDesign method.

--- server/src/design-file-utils/createDesign.js
+++ server/src/design-file-utils/createDesign.js
@@ -2,17 +2,22 @@ import fs from 'fs/promises';
 import path from "node:path";
 import {DALEngine} from "dal-engine-core-js-lib-dev";
 
-async function createDesign(fileName) {
+/**
+ * Creates a design with the name in the workspace (if it doesn't exist).
+ * The design is initialized as an engine with the name provided.
+ * @param {String} designName 
+ */
+async function createDesign(designName) {
     try {
-        if (!fileName) {
-            throw new Error("File name is required to create a design.");
+        if (!designName) {
+            throw new Error("Design name is required to create a design.");
         }
         const engine = new DALEngine({
-            name: "default",
+            name: designName,
             description: "Default engine",
         });
         const workspacePath = path.join(process.cwd(), "workspace");
-        const name = path.join(workspacePath, fileName);
+        const name = path.join(workspacePath, designName);
 
         await fs.writeFile(name, engine.serialize(), { flag: "wx" });
     } catch (err) {
