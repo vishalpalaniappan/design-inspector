06c092c0520696d624adbdb9bda24501d952fb57
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-11 19:06:33 -0400

    Modify playground loader to unzip contents into playground.

--- server/src/design-file-utils/loadDesignInPlayground.js
+++ server/src/design-file-utils/loadDesignInPlayground.js
@@ -1,6 +1,7 @@
 import path from 'path';
 import { clearPlaygroundFolder } from "./initFolders.js";
-import fs from 'fs/promises';
+import instrumentationRunner from '../runners/instrumentationRunner.js';
+import unzipper from "unzipper";
 
 async function loadDesignInPlayground(engine) {
     // Get files from engine.
@@ -16,22 +17,26 @@ async function loadDesignInPlayground(engine) {
     const instrumentationPkg = engine.implementation.exportForInstrumentation();
 
     // Temporarily write to temp folder for accesing instrumentation package
-    await fs.writeFile(path.join("temp",engine._name + ".json"), instrumentationPkg);
-    
-    // TODO:
-    // Call runner here and accept the returned instrumented sources.
+    // await fs.writeFile(path.join("temp",engine._name + ".json"), instrumentationPkg);
 
-    await Promise.all(files.map(async (file) => {
-        // TODO: Replace file name with key to create subfolders For now all files
-        // exist on the same level with unique names. This requires changes to
-        // the create file functions so it can create directories and not just files.
-        const filePath = path.resolve(playgroundPath, file.getName());
-        if (!filePath.startsWith(playgroundPath + path.sep)) {
-            throw new Error(`Invalid file path: ${file.getName()}`);
-        }
-        await fs.mkdir(path.dirname(filePath), { recursive: true });
-        await fs.writeFile(filePath, file.getContent(), { flag: "w" });
-    }));
+    try {
+        const zip = await instrumentationRunner(instrumentationPkg);
+        const directory = await unzipper.Open.buffer(zip);
+        await directory.extract({ path: playgroundPath });
+    } catch (error) {
+        throw error;
+    }
+    // await Promise.all(files.map(async (file) => {
+    //     // TODO: Replace file name with key to create subfolders For now all files
+    //     // exist on the same level with unique names. This requires changes to
+    //     // the create file functions so it can create directories and not just files.
+    //     const filePath = path.resolve(playgroundPath, file.getName());
+    //     if (!filePath.startsWith(playgroundPath + path.sep)) {
+    //         throw new Error(`Invalid file path: ${file.getName()}`);
+    //     }
+    //     await fs.mkdir(path.dirname(filePath), { recursive: true });
+    //     await fs.writeFile(filePath, file.getContent(), { flag: "w" });
+    // }));
 }
 
 
