2e74cdf817688b360b7a58f6790be30b5293993d
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-11 19:42:10 -0400

    Switch to unzipping to playground.

--- server/src/design-file-utils/loadDesignInPlayground.js
+++ server/src/design-file-utils/loadDesignInPlayground.js
@@ -2,6 +2,7 @@ import path from 'path';
 import { clearPlaygroundFolder } from "./initFolders.js";
 import instrumentationRunner from '../runners/instrumentationRunner.js';
 import unzipper from "unzipper";
+import fs from 'fs/promises';
 
 async function loadDesignInPlayground(engine) {
     // Get files from engine.
@@ -13,11 +14,12 @@ async function loadDesignInPlayground(engine) {
     // Write engine files to playground folder
     const playgroundPath = path.join(process.cwd(), "playground");
 
-
     const instrumentationPkg = engine.implementation.exportForInstrumentation();
 
     // Temporarily write to temp folder for accesing instrumentation package
-    // await fs.writeFile(path.join("temp",engine._name + ".json"), instrumentationPkg);
+    const filePath = path.join("temp", engine._name + ".json");
+    await fs.mkdir(path.dirname(filePath), { recursive: true });
+    await fs.writeFile(filePath, instrumentationPkg);
 
     try {
         const zip = await instrumentationRunner(instrumentationPkg);
@@ -26,17 +28,6 @@ async function loadDesignInPlayground(engine) {
     } catch (error) {
         throw error;
     }
-    // await Promise.all(files.map(async (file) => {
-    //     // TODO: Replace file name with key to create subfolders For now all files
-    //     // exist on the same level with unique names. This requires changes to
-    //     // the create file functions so it can create directories and not just files.
-    //     const filePath = path.resolve(playgroundPath, file.getName());
-    //     if (!filePath.startsWith(playgroundPath + path.sep)) {
-    //         throw new Error(`Invalid file path: ${file.getName()}`);
-    //     }
-    //     await fs.mkdir(path.dirname(filePath), { recursive: true });
-    //     await fs.writeFile(filePath, file.getContent(), { flag: "w" });
-    // }));
 }
 
 
