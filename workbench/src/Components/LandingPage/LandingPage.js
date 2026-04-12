8906b91ba97e2a3ab96979a3880ea5a33726349d
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-12 16:37:20 -0400

    Add second layout for debugging mode.

--- workbench/src/Components/LandingPage/LandingPage.js
+++ workbench/src/Components/LandingPage/LandingPage.js
@@ -1,10 +1,14 @@
-import React, {useCallback} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 
+import {useDispatch} from "react-redux";
 import {LayoutManager} from "ui-layout-manager-dev";
 
 import layout from "../../layout.json";
+import layout2 from "../../layout2.json";
 import {registry} from "../../Registry";
+import {setDesignLoaded} from "../../Store/appSlice";
 import {useDesignLoaded} from "../../Store/useAppSelection";
+import {useAppMode} from "../../Store/useAppSelection";
 import {LoadDesign} from "./LoadDesign/LoadDesign";
 
 import "./LandingPage.scss";
@@ -18,13 +22,28 @@ import "./LandingPage.scss";
 export function LandingPage () {
     const designLoaded = useDesignLoaded();
 
+    const appMode = useAppMode();
+    const dispatch = useDispatch();
+
+    const [chosenLayout, setChosenLayout] = useState(layout);
+
     const registryList = useCallback(() => registry, []);
+
+    useEffect(() => {
+        if (appMode === 1) {
+            dispatch(setDesignLoaded(false));
+            setChosenLayout(layout);
+        } else if (appMode === 2) {
+            dispatch(setDesignLoaded(false));
+            setChosenLayout(layout2);
+        }
+    }, [appMode]);
 
     return (
         <>
             {
                 designLoaded?
-                    <LayoutManager registry={registryList()} ldf={layout} /> :
+                    <LayoutManager registry={registryList()} ldf={chosenLayout} /> :
                     <LoadDesign />
             }
         </>
