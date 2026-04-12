154405d45b378065db73d36bda6ffdc60bb6eee4
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-12 16:37:57 -0400

    Add select to switch between modes.

--- workbench/src/Components/StatusBar/StatusBar.js
+++ workbench/src/Components/StatusBar/StatusBar.js
@@ -1,9 +1,13 @@
 import React, {useContext, useEffect, useRef, useState} from "react";
 
 import {CircleFill} from "react-bootstrap-icons";
+import {useDispatch} from "react-redux";
 
 import ServerContext from "../../Providers/ServerContext";
+import {setDebuggingMode} from "../../Store/appSlice";
+import {setDesignMode} from "../../Store/appSlice";
 import {useStatusMsg} from "../../Store/useAppSelection";
+import {useAppMode} from "../../Store/useAppSelection";
 
 import "./StatusBar.scss";
 
@@ -14,6 +18,8 @@ import "./StatusBar.scss";
 export function StatusBar () {
     const {connectionStatus} = useContext(ServerContext);
     const timeoutRef = useRef(null);
+    const appMode = useAppMode();
+    const dispatch = useDispatch();
 
     const [connectionColor, setConnectionColor] = useState({color: "green"});
     const [message, setMessage] = useState("");
@@ -43,12 +49,29 @@ export function StatusBar () {
         setConnectionColor({color: connectionColorMap[connectionStatus] || "red"});
     }, [connectionStatus]);
 
+    const selectMode = (event) => {
+        const value = parseInt(event.target.value);
+        if (value === 1 && appMode !== 1) {
+            console.log("Design mode selected");
+            dispatch(setDesignMode());
+        } else if (value === 2) {
+            console.log("Debugging mode selected");
+            dispatch(setDebuggingMode());
+        }
+    };
+
     return (
         <div className="status-bar">
             <div className="status-left">
                 {message}
             </div>
             <div className="status-right">
+                <div className="status-bar-select">
+                    <select value={appMode} onChange={(event) => selectMode(event)}>
+                        <option value={1}>Design</option>
+                        <option value={2}>Debugging</option>
+                    </select>
+                </div>
                 <CircleFill
                     className="connectionColor"
                     style={connectionColor} />{connectionStatus}
