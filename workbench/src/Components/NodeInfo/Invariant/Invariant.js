9af44ace8dad2d072535b941476e24160e9315a8
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-13 20:59:10 -0400

    Open add prediction modal when icon is clicked.

--- workbench/src/Components/NodeInfo/Invariant/Invariant.js
+++ workbench/src/Components/NodeInfo/Invariant/Invariant.js
@@ -3,10 +3,12 @@ import React, {useCallback, useEffect, useState} from "react";
 import PropTypes from "prop-types";
 import {PlusSquare, Trash} from "react-bootstrap-icons";
 import {useDispatch} from "react-redux";
+import {useModalManager} from "ui-layout-manager-dev";
 
 import {setSelectedInvariant} from "../../../Store/appSlice";
 import {deleteInvariantThunk} from "../../../Store/appThunk";
 import {useSelectedInvariant} from "../../../Store/useAppSelection";
+import {AddPrediction} from "../../Modals/AddPrediction";
 
 import "./Invariant.scss";
 
@@ -20,6 +22,7 @@ Invariant.propTypes = {
  */
 export function Invariant ({invariant}) {
     const dispatch = useDispatch();
+    const {openModal} = useModalManager();
 
     const [selected, setSelected] = React.useState(false);
     const selectedInvariant = useSelectedInvariant();
@@ -46,11 +49,23 @@ export function Invariant ({invariant}) {
         setSelected(selectedInvariant.getName() === invariant.getName());
     }, [selectedInvariant, invariant]);
 
+    const addPrediction = useCallback(() => {
+        openModal({
+            title: "Add Prediction",
+            render: ({close}) => {
+                return <AddPrediction close={close} />;
+            },
+        });
+    }, [openModal]);
+
     return (
         <div className={`participantCard ${selected ? "selected" : ""}`}
             onClick={selectInvariant}>
             <div className="label">
-                <PlusSquare size={13} title={"Add Prediction"} className="icon"/>
+                <PlusSquare size={13}
+                    title={"Add Prediction"}
+                    className="icon"
+                    onClick={addPrediction}/>
                 {invariant.getName()}
             </div>
             <div className="icons">
