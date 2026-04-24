3074c7ed33ecf2d860eb85a341964929a0569cf7
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-24 04:50:32 -0400

    Fix selecting behavior id in scripting slice.

--- workbench/src/Store/scriptingSlice/scriptingSelectors.js
+++ workbench/src/Store/scriptingSlice/scriptingSelectors.js
@@ -3,3 +3,4 @@ export const selectScripts = (state) => state.scripting.scripts;
 export const selectTransformOutput = (state) => state.scripting.transformOutput;
 export const selectTransformOutputLog = (state) => state.scripting.transformOutputLog;
 export const selectScriptingCounter = (state) => state.scripting.counter;
+export const selectScriptingBehaviorId = (state) => state.app.selectedBehavior;
