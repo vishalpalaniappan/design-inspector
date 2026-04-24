3c8daba2c773c04185564b6e01d67c33246a759d
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-24 07:15:10 -0400

    Remove unused states.

--- workbench/src/Store/scriptingSlice/scriptingSlice.js
+++ workbench/src/Store/scriptingSlice/scriptingSlice.js
@@ -4,36 +4,21 @@ import { addTraceThunk } from "../appThunk";
 const scriptingSlice = createSlice({
     name: "scripting",
     initialState: {
-        scripts: {
-            initialArgs: "",
-            initialWorldState: "",
-            expectedPostWorldState: "",
-            primitives: "",
-            computedPostWorldState: "",
-        },
         transformOutput: null,
         transformOutputLog: [],
         scriptingCounter: 0,
     },
     reducers: {
-        setScript(state, action) {
-            const {scriptType, content} = action.payload;
-            if (state.scripts.hasOwnProperty(scriptType)) {
-                state.scripts[scriptType] = content;
-            } else {
-                console.warn(`Unknown script type: ${scriptType}`);
-            }
-        },
-        setTransformOutput(state, action) {
+        setTransformOutput (state, action) {
             state.transformOutput = action.payload;
         },
-        setTransformOutputLog(state, action) {
+        setTransformOutputLog (state, action) {
             state.transformOutputLog = action.payload;
         },
-        addTransformOutputLog(state, action) {
+        addTransformOutputLog (state, action) {
             state.transformOutputLog.push(action.payload);
         },
-        incrementScriptingCounter(state) {
+        incrementScriptingCounter (state) {
             state.scriptingCounter += 1;
         },
     },
@@ -41,7 +26,6 @@ const scriptingSlice = createSlice({
 
 export const {
     addTransformOutputLog,
-    setScript,
     setTransformOutput,
     setTransformOutputLog,
     incrementScriptingCounter,
