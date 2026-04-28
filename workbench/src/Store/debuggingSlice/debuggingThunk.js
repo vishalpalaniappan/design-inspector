b039f0ff861326ddf01d2473461d7bb8f22ff876
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-04-28 17:34:12 -0400

    Fix setSelectedTraceEntryThunk

--- workbench/src/Store/debuggingSlice/debuggingThunk.js
+++ workbench/src/Store/debuggingSlice/debuggingThunk.js
@@ -1,5 +1,4 @@
 // debugging slice thunk
-import {selectSelectedTraceId} from "./debuggingSelectors";
 import {setSelectedTraceEntry} from "./debuggingSlice";
 
 export const setSelectedTraceEntryThunk = (entryIndex) => (dispatch, getState) => {
@@ -7,6 +6,5 @@ export const setSelectedTraceEntryThunk = (entryIndex) => (dispatch, getState) =
         console.warn("Invalid trace ID provided.");
         return;
     }
-    const traceId = selectSelectedTraceId(getState());
-    dispatch(setSelectedTraceEntry(traceId));
+    dispatch(setSelectedTraceEntry(entryIndex));
 };
