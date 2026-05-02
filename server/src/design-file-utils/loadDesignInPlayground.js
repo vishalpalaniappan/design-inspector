40e970d7eb0f1d56be2c5170cba266daca6ab231
Vishal Palaniappan <vishal.palaniappan@yscope.com>
2026-05-01 20:41:49 -0400

    Add note on live debugging.

--- server/src/design-file-utils/loadDesignInPlayground.js
+++ server/src/design-file-utils/loadDesignInPlayground.js
@@ -2,6 +2,19 @@ import path from 'path';
 import { clearPlaygroundFolder } from "./initFolders.js";
 import fs from 'fs/promises';
 
+/**
+ * TODO:
+ * If I want to implement live debugging, I  have to implement the debugger as a state machine
+ * where the inputs are each log statement. This means that as the logs stream in, the state machine
+ * will transition. I don't wait for the entire trace to debug, I do it live as the state machine 
+ * transitions. Its actually a very simple implementation if the setup is done right, so I'm not
+ * bothering with it yet, it needs changes to the UI, the engine and adding infrastructure to stream
+ * the logs as they are generated. I will do it later since it isn't priority. I'm not sure if this
+ * is the right place to leave this note, but I will just leave it here. I should probably open an
+ * issue with this information, that seems like the best way to track it, so I will migrate all these
+ * kinds of todos to git later.
+ */
+
 async function loadDesignInPlayground(engine) {
 
     /**
